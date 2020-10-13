50b9d2e45c56d0e818cf03ebad6b5420f754ff8f
underwoodbt <36803636+underwoodbt@users.noreply.github.com>
2020-10-13 16:31:52 -0400

    Update p06.py

--- p06.py
+++ p06.py
@@ -48,13 +48,13 @@ class Pet:
 class Canine(Animal):
     def roam(self):
         print('canines like to roam in packs...')
-        # TODO: update hunger level
+        self.set_hunger_level(self.get_hunger_level() + 1)
 
 
 class Feline(Animal):
     def roam(self):
         print('felines like to roam alone...')
-        # TODO: update hunger level
+        self.set_hunger_level(self.get_hunger_level() + 1)
 
 
 class Wolf(Canine):
@@ -63,13 +63,17 @@ class Wolf(Canine):
 
     def eat(self):
         print('rip with teeth...')
+        self.set_hunger_level(self.get_hunger_level() - 2)
+
         
 class Cat(Feline, Pet):
     def make_noise(self):
         print('meow...')
+        
 
     def eat(self):
         print('pick...')
+        self.set_hunger_level(self.get_hunger_level() - 3)
     
     def play(self):
         print('scracth...')
@@ -78,15 +82,17 @@ class Dog(Canine, Pet):
     def make_noise(self):
         print('bark...')
 
-    def eat():
-        print('rip with teeth')
+    def eat(self):
+        print('slop...')
+        self.set_hunger_level(self.get_hunger_level() - 3)
 
 class Lion(Feline):
     def make_noise(self):
         print('roar...')
     
-    def eat():
+    def eat(self):
         print('rip with teeth...')
+        self.set_hunger_level(self.get_hunger_level() - 3)
 
 class Hippo(Animal):
     def make_noise(self):
@@ -94,6 +100,7 @@ class Hippo(Animal):
 
     def eat(self):
         print('slurp...')
+        self.set_hunger_level(self.get_hunger_level() - 1)
 
 
 class Zoo:
@@ -101,14 +108,27 @@ class Zoo:
         self._name = name
         self._zoo_animals = []
 
-    # TODO: add the "add animal" method
+    def add_animal(self, animal):
+        self._zoo_animals.append(animal)
 
-    # TODO: "test animals" method
+    def test_animal(self):
+        print('zoo name: ' + self._name)
+        #print(len('number of animals: ' + self._zoo_animals))
+
+        for i in self._zoo_animals:
+            print(i.get_name(self))
+            print(i.sleep(self))
+
+
+
+
 
 
 # TODO: main function
 def main():
-    pass
+    zoo = Zoo('zoo')
+    zoo.add_animal(Dog)
+    zoo.test_animal()
 
 
 if __name__ == '__main__':
